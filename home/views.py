c475dd2a7dfd50d33c9715903da75962937b20c2
Zhao <zhaoshangming1997@gmail.com>
2017-07-03 21:03:16 +0800

    able to search by comment

--- home/views.py
+++ home/views.py
@@ -3,8 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 from .models import Shop, Comment
 from .forms import SearchForm
-from django import forms
 import json
+import jieba
+import gensim
+from pre_process.generate_index_file_defs import get_stop_words
+
+
+dictionary_file = 'pre_process/comment_dictionary.dict'
+lsi_file = 'pre_process/comment_lsi.lsi'
+index_file = 'pre_process/comment_index.index'
+shop_ids_file = 'pre_process/shop_ids.txt'
+comment_file = 'pre_process/comments.txt'
+stop_words_file = 'pre_process/stop_words.txt'
 
 
 def home(request):
@@ -13,28 +23,17 @@ def home(request):
         if search_form.is_valid():
             search_choice = search_form.cleaned_data['search_choice']
             sort_choice = search_form.cleaned_data['sort_choice']
-            char_input = search_form.cleaned_data["search_input"]
-            if search_choice == 'LOC' or search_choice == 'FOODTYPE':
-                return render(request, 'home/home.html', {'search_form': search_form, 'search_choice': search_choice, 'char_input': char_input,
-                                                          'shops': search_by_property(search_choice, sort_choice, char_input)})
-            elif search_choice == 'COMMENT':
-                return render(request, 'home/home.html', {'search_form': search_form})
+            search_input = search_form.cleaned_data["search_input"]
+            if search_choice == 'LOC' or search_choice == 'FOODTYPE':  # search by property
+                return render(request, 'home/home.html', {'search_form': search_form, 'search_choice': search_choice, 'char_input': search_input,
+                                                          'shops': search_by_property(search_choice, sort_choice, search_input)})
+            elif search_choice == 'COMMENT':  # search by content
+                comments, shops = search_by_comment(search_input, sort_choice)
+                return render(request, 'home/home.html', {'search_form': search_form,
+                                                          'comments': comments, 'shops': shops})
             return render(request, 'home/home.html', {'search_form': search_form})
     search_form = SearchForm()
     return render(request, 'home/home.html', {'search_form': search_form})
-
-
-'''def search(request):
-    if request.method == 'POST':
-        search_choice_form = SearchChoiceForm(request.POST)
-        if search_choice_form.is_valid():
-            search_choice = search_choice_form.cleaned_data['search_choice']
-            sort_choice = search_choice_form.cleaned_data['sort_choice']
-            if search_choice == 'LOC' or search_choice == 'FOODTYPE':
-                search_by_property(request, search_choice, sort_choice)
-            elif search_choice == 'COMMENT':
-                search_by_comment(request, sort_choice)
-    return render(request, 'home/home.html')'''
 
 
 def search_by_property(search_choice, sort_choice, char_input):
@@ -68,10 +67,35 @@ def show_statistics(request, search_choice, char_input):
     return render(request, 'home/statistics.html', {'statistics': json.dumps(statistics_json)})
 
 
-'''def search_by_comment(request, sort_choice):
-    if request.method == "POST":
-        text_input_form = TextForm(request.POST)
-        if text_input_form.is_valid():
-            return render(request, 'home/home.html', {'text_input_form': text_input_form})
-    text_input_form = TextForm()
-    return render(request, 'home/home.html', {'text_input_form': text_input_form})'''
+def search_by_comment(search_input, sort_choice):
+    # pre-procession
+    words = jieba.cut(search_input)
+    stop_words_list = get_stop_words(stop_words_file)
+    words = [word for word in words if not word in stop_words_list]
+    dictionary = gensim.corpora.Dictionary.load(dictionary_file)
+    lsi = gensim.models.LsiModel.load(lsi_file)
+    query_lsi = lsi[dictionary.doc2bow(words)]
+
+    index = gensim.similarities.Similarity.load(index_file)
+    sims = index[query_lsi]
+    max_shop_num = 50
+    sims = sorted(enumerate(sims), key=lambda item: -item[1])[:max_shop_num]
+
+    comments = []
+    with open(comment_file) as f:
+       lines = f.readlines()
+       comments = [lines[item[0]] for item in sims]
+    shop_ids = []
+    with open(shop_ids_file) as f:
+       lines = f.readlines()
+       shop_ids = [int(lines[item[0]]) for item in sims]
+    shops = Shop.objects.filter(pk__in=shop_ids).order_by(sort_choice.lower())
+    return comments, shops
+
+
+
+
+
+
+
+
