d031ff5e56c0e960fecacd6ffc6b22e9dd961ff5
AliceMa-thss15 <thss15_malj@163.com>
2017-07-04 23:53:43 +0800

    success

--- single_shop/models.py
+++ single_shop/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+from home.models import ShopManager, Shop, Comment
+
+
+
+
--- single_shop/views.py
+++ single_shop/views.py
@@ -2,7 +2,22 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Shop, Comment
+from django import forms
+import json
 
 
-def single_shop(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def single_shop(request, urlID):
+    return render(request, 'single_shop/single_shop.html', {'shop': get_shop_by_id(urlID)})
+
+    #if request.method == 'POST':
+    #    pass
+    #return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def get_shop_by_id(urlID):
+    shop = Shop.objects.get(urlID=urlID)
+    return shop
+
+
+
